c236e7e386e0ece9ef7cd3a935b4221a77185d6d
Dominique Chin-Jones <domecj13@gmail.com>
2022-11-14 15:51:57 -0500

    testing message logic

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,18 @@ import { useState } from "react";
 import "./App.css";
 
 export default function App() {
+  const messageOne = {
+    user: "sender",
+    message: "hello",
+  };
+
+  const messageTwo = {
+    user: "receiver",
+    message: "reply",
+  };
+
+  const messageList = [messageOne, messageTwo];
+
   return (
     <div className="container">
       <div className="header">
@@ -9,6 +21,13 @@ export default function App() {
           <h1>Chat App</h1>
         </header>
         <span>X</span>
+      </div>
+      <div className="message-container">
+        {messageList.map((message) => (
+          <p key={message.user} className={message.user}>
+            {message.message}
+          </p>
+        ))}
       </div>
     </div>
   );
